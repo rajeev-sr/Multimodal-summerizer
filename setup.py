65aab50c5878acc4532e3058dae5272ec5846c96
RAJEEV KUMAR <rajeevkumar16034@gmail.com>
2025-07-07 18:11:50 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="project_name",
+    version="0.0.1",
+    author="Rajeev",
+    author_email="rajeevkumar16034@gmail.com",
+    packages=find_packages(),
+    install_requires=["langchain"]
+)
