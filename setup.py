7f5002efc07681bf542ae857301a1ed9ba416699
rajeev-sr <rajeevkumar16034@gmail.com>
2025-07-08 16:26:07 +0530

    add multimodal notebook

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="project_name",
+    name="Multimodal-Summerizer",
     version="0.0.1",
     author="Rajeev",
     author_email="rajeevkumar16034@gmail.com",
